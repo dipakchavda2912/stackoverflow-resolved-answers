c1b45df1ee41f7517b1405d60b4e3ee59b90b362
Dipak Chavda <dipak.chavda2912@gmail.com>
2018-02-02 17:09:40 +0530

    stackoverflow-48580200
    https://stackoverflow.com/questions/48580200/switch-video-position-and-play-loop/48581477#48581477

---
+++ stackoverflow-48580200/script.js
@@ -0,0 +1,43 @@
+// Code goes here
+
+$(document).ready(function() {
+
+      var computeTime = 0;
+      var bottomComputeTime = 0;
+      var LoopComputeLimit = 2;
+
+      var divVideo = $("<div/>")
+        .attr({
+          class: "divVideo"
+        }).html("<video id=\"myVideo\" controls autoplay width=\"300\" height=\"200\"><source src=\"http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4\" type=\"video/mp4\"></video>");
+
+      resetPositionToTop();
+
+      $("#myVideo").unbind("ended").bind("ended", function(e) {
+        
+          console.log("bottomComputeTime:", bottomComputeTime, "computeTime:", computeTime);
+
+            if (computeTime === 0) {
+              resetPositionToBottom();
+            }
+
+            if (bottomComputeTime < LoopComputeLimit) {
+              bottomComputeTime++;
+              $("#myVideo").get(0).play();
+            } else if (bottomComputeTime === LoopComputeLimit) {
+              bottomComputeTime = 0;
+              computeTime = 0;
+              resetPositionToTop();
+              $("#myVideo").get(0).play();
+            }
+          });
+
+        function resetPositionToTop() {
+          divVideo.appendTo(".top");
+        }
+
+        function resetPositionToBottom() {
+          divVideo.appendTo(".bottom");
+        }
+
+      });
